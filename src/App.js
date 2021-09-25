2088cc6ad80a52cf59dd3bebec11f4a552401fff
saifulislammunna <saifulmunna28@gmail.com>
2021-09-26 00:13:41 +0600

    fake-data & header added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+ 
+import { useEffect } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
+  useEffect( () => {
+    fetch('./writers.JSON')
+    .then(res => res.json())
+    .then(data => console.log(data));
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/data/writers.js
@@ -0,0 +1,90 @@
+const writes = [
+  {
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/LewisCarrollSelfPhoto.jpg/220px-LewisCarrollSelfPhoto.jpg',
+      name: 'Lewis Carroll',
+      born: '27 January 1832',
+      education:'Rugby School, University of Oxford',
+      occupation:'Author',
+      books : 10 
+  },
+  {
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/LewisCarrollSelfPhoto.jpg/220px-LewisCarrollSelfPhoto.jpg',
+      name: 'Lewis Carroll',
+      born: '27 January 1832',
+      education:'Rugby School, University of Oxford',
+      occupation:'Author',
+      books : 10 
+  },
+  {
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/LewisCarrollSelfPhoto.jpg/220px-LewisCarrollSelfPhoto.jpg',
+      name: 'Lewis Carroll',
+      born: '27 January 1832',
+      education:'Rugby School, University of Oxford',
+      occupation:'Author',
+      books : 10 
+  },
+  {
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/LewisCarrollSelfPhoto.jpg/220px-LewisCarrollSelfPhoto.jpg',
+      name: 'Lewis Carroll',
+      born: '27 January 1832',
+      education:'Rugby School, University of Oxford',
+      occupation:'Author',
+      books : 10 
+  },
+  {
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/LewisCarrollSelfPhoto.jpg/220px-LewisCarrollSelfPhoto.jpg',
+      name: 'Lewis Carroll',
+      born: '27 January 1832',
+      education:'Rugby School, University of Oxford',
+      occupation:'Author',
+      books : 10 
+  },
+  {
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/LewisCarrollSelfPhoto.jpg/220px-LewisCarrollSelfPhoto.jpg',
+      name: 'Lewis Carroll',
+      born: '27 January 1832',
+      education:'Rugby School, University of Oxford',
+      occupation:'Author',
+      books : 10 
+  },
+  {
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/LewisCarrollSelfPhoto.jpg/220px-LewisCarrollSelfPhoto.jpg',
+      name: 'Lewis Carroll',
+      born: '27 January 1832',
+      education:'Rugby School, University of Oxford',
+      occupation:'Author',
+      books : 10 
+  },
+  {
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/LewisCarrollSelfPhoto.jpg/220px-LewisCarrollSelfPhoto.jpg',
+      name: 'Lewis Carroll',
+      born: '27 January 1832',
+      education:'Rugby School, University of Oxford',
+      occupation:'Author',
+      books : 10 
+  },
+  {
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/LewisCarrollSelfPhoto.jpg/220px-LewisCarrollSelfPhoto.jpg',
+      name: 'Lewis Carroll',
+      born: '27 January 1832',
+      education:'Rugby School, University of Oxford',
+      occupation:'Author',
+      books : 10 
+  },
+  {
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/LewisCarrollSelfPhoto.jpg/220px-LewisCarrollSelfPhoto.jpg',
+      name: 'Lewis Carroll',
+      born: '27 January 1832',
+      education:'Rugby School, University of Oxford',
+      occupation:'Author',
+      books : 10 
+  },
+  {
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/LewisCarrollSelfPhoto.jpg/220px-LewisCarrollSelfPhoto.jpg',
+      name: 'Lewis Carroll',
+      born: '27 January 1832',
+      education:'Rugby School, University of Oxford',
+      occupation:'Author',
+      books : 10 
+  },
+]
